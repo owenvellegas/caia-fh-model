17f9397d653106dfd3c12cad5d5c91825dd1a6c5
Owen Vellegas <ovellegas@mac.mynetworksettings.com>
2025-12-17 20:23:06 -0800

    main.py

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
+import pandas as pd
 import os
 from dotenv import load_dotenv
 from src.load_data import load_data
+from models.six_month_lr import six_month_lr
+from models.twelve_month_lr import twelve_month_lr
+from models.six_month_rf import six_month_rf
+from models.twelve_month_rf import twelve_month_rf
 
 load_dotenv()
 
 FOLDER_PATH = os.getenv("FOLDER_PATH")
 
-load_data(FOLDER_PATH)
+df_features = load_data(FOLDER_PATH)
 
+six_month_lr(df_features)
+twelve_month_lr(df_features)
+six_month_rf(df_features)
+twelve_month_rf(df_features)
